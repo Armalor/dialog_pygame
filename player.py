92df7a6d548c7ec47701129a1a62a1f97d1001f1
NNK <n.kutrukhin@digitalms.ru>
2025-07-19 01:01:51 +0300

    Космический корабль и выстрел

---
+++ player.py
@@ -0,0 +1,20 @@
+import pygame
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+WIDTH = 1024   # ширина игрового окна
+HEIGHT = 768  # высота игрового окна
+FPS = 30  # частота кадров в секунду
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
