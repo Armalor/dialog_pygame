b6fa2400bf649b6a0c1b5dbd5dc95a8d28c7ec75
diar3 <arseniy.apelsinov@gmail.com>
2025-07-20 10:06:07 +0300

    1 commit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import pygame
 import random
 
+import pygame
+from time import perf_counter
 from spaceship import Spaceship
+from spaceship import Bullet, bullets, Enemy, enemies
 
-WIDTH = 1200   # ширина игрового окна
-HEIGHT = 1000  # высота игрового окна
-FPS = 30  # частота кадров в секунду
+WIDTH = 1200
+HEIGHT = 700
+FPS = 30
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -13,14 +15,13 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+t0 = perf_counter()
+
 if __name__ == '__main__':
     # создаем игру и окно
     pygame.init()
-    pygame.mixer.init()  # для звука
+    pygame.mixer.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-    # Внимание! Мы можем создавать класс Spaceship только ПОСЛЕ инициализации screen!
-
     ship = Spaceship(velocity=20, screen=screen)
 
     pygame.display.set_caption("My Game")
@@ -29,24 +30,34 @@ if __name__ == '__main__':
     running = True
     while running:
         clock.tick(FPS)
-
-        # Рендеринг
         screen.fill(BLACK)
 
+        drawing_bullet = False  # рисовать ли очередную пулю
         for event in pygame.event.get():
-            # проверить закрытие окна
             if event.type == pygame.QUIT:
                 running = False
-
-            # if event.type == pygame.KEYDOWN:
-            #     if event.key == pygame.K_RIGHT:
-            #         color = RED
-            # else:
-            #     color = GREEN
+            if event.type == pygame.KEYUP:       # Рисовать пулю, только если отпущен пробел!!!
+                if event.key == pygame.K_SPACE:  #                                            |
+                    drawing_bullet = True  # <------------------------------------------------
 
         keys = pygame.key.get_pressed()
+
+        if drawing_bullet:
+            # Добавление новой пули в список пуль (немного подгоняются координаты)
+            Bullet(-10, ship.texture_rect.center[0] + 1, ship.texture_rect.center[1] - 70, screen)
+
+        if int(t0) % 5 == 0:
+            Enemy(5, random.randint(100, WIDTH - 100), 100, screen)
+
+        for bullet in bullets:  # проходимся по всем пулям, двигаем их и рисуем
+            bullet.move()
+            bullet.draw()
+
+        for enemy in enemies:
+            enemy.draw()
+            enemy.move()
+
         ship.move(keys)
         ship.draw()
 
-        # после отрисовки всего, переворачиваем экран
         pygame.display.flip()
--- spaceship.py
+++ spaceship.py
@@ -2,44 +2,67 @@ import pygame
 from pygame.surface import Surface
 from pygame.key import ScancodeWrapper
 
+bullets = []
+enemies = []
 
 class Bullet:
     def __init__(self, velocity, x, y, screen: Surface):
         self.texture = pygame.image.load("images/blaster/blaster_1_85.png").convert_alpha()
         self.texture_rect = self.texture.get_rect()
-
         self.texture_rect.center = (x, y - self.texture_rect.height // 2)
+        self.screen = screen
+        self.velocity = velocity
 
+        bullets.append(self)
+
+    def draw(self):
+        self.screen.blit(self.texture, self.texture_rect)
+
+    def move(self):
+        if self.texture_rect.bottom > 0:
+            self.texture_rect.move_ip(0, self.velocity)
+        else:
+            bullets.remove(self)
 
 class Spaceship:
     def __init__(self, velocity, screen: Surface):
         self.texture = pygame.image.load("images/spaceship3_200.png").convert_alpha()
         self.texture_rect = self.texture.get_rect()
-
         self.screen = screen
         screen_width, screen_height = screen.get_size()
 
         self.texture_rect.center = (screen_width // 2, screen_height - self.texture_rect.height // 2 - 20)
-
         self.velocity = velocity
-
-        self.bullet = Bullet(25, self.texture_rect.center[0], self.texture_rect.top, self.screen)
 
     def move(self, keys: ScancodeWrapper):
         if keys[pygame.K_LEFT]:
             if self.texture_rect.left > 0:
                 self.texture_rect.move_ip(-self.velocity, 0)
-                #
-                self.bullet.texture_rect.move_ip(0, -self.velocity)
 
         if keys[pygame.K_RIGHT]:
             screen_width, _ = self.screen.get_size()
             if self.texture_rect.right < screen_width:
                 self.texture_rect.move_ip(+self.velocity, 0)
-                #
-                self.bullet.texture_rect.move_ip(0, -self.velocity)
 
     def draw(self):
-        # Отображение текстуры
         self.screen.blit(self.texture, self.texture_rect)
-        self.screen.blit(self.bullet.texture, self.bullet.texture_rect)
+
+
+class Enemy:
+    def __init__(self, velocity, x, y, screen: Surface):
+        self.texture = pygame.image.load("images/prisheleccc.png").convert_alpha()
+        self.texture_rect = self.texture.get_rect()
+        self.texture_rect.center = (0, 100)
+        self.screen = screen
+        self.velocity = velocity
+
+        enemies.append(self)
+
+    def draw(self):
+        self.screen.blit(self.texture, self.texture_rect)
+
+    def move(self):
+        if self.texture_rect.bottom > 0:
+            self.texture_rect.move_ip(0, self.velocity)
+        else:
+            enemies.remove(self)
