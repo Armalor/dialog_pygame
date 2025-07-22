8bc71e777ad16308ab3598279e5a6bbd2aa2706c
diar3 <noglushkov@gmail.com>
2025-07-22 10:12:23 +0300

    более менее готово

---
+++ example_1_client.py
@@ -0,0 +1,40 @@
+import socket
+from threading import Thread, Lock
+from time import sleep
+import random
+from net import Net
+
+
+blocker = Lock()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # создаем сокет
+
+
+def in_thread(stop=1000_000, name=None):
+    while True:
+        data = sock.recv(1024)  # читаем ответ от серверного сокета
+        print(data.decode())
+
+
+if __name__ == '__main__':
+
+
+    client = Net.get_local_ip()
+    print(f'local IP: {client}')
+    host = Net.scan()
+    if host:
+        print(f'server host: {host}')
+        sock.connect((host, Net.SERVER_PORT))  # подключемся к серверному сокету
+
+        t = Thread(target=in_thread, args=(sock,), daemon=True)
+        t.start()
+
+        while True:
+            message = input()
+            sock.send(bytes(message, encoding='UTF-8'))
+
+
+
+
+
+
+    sock.close()
